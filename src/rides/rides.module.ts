d3e33a4d4b7d8077496ed603d831d5561aa042d8
BETO MENDES <134823871+mendesbeto@users.noreply.github.com>
2026-09-20 00:41:47 +0100

    chore: inject wallet into rides module

--- src/rides/rides.module.ts
+++ src/rides/rides.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RidesController } from './rides.controller';
 import { RidesService } from './rides.service';
+import { WalletModule } from '../wallet/wallet.module';
 
 @Module({
+  imports: [WalletModule],
   controllers: [RidesController],
   providers: [RidesService],
 })
